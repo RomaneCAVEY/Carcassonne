99fc2ac1224758742c1f2d38dd216095975b2bf5
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2024-03-22 17:53:22 +0100

    Structure player_1

---
+++ src/player1.c
@@ -0,0 +1,37 @@
+#include "player.h"
+#include "board.h"
+
+ board_t raph_1={};
+
+char const* get_player_name(){
+	return "Player_1";
+}
+
+void initialize(unsigned int player_id, const struct move_t first_move, struct gameconfig_t config) {
+	graph_1= init_board(first_move);
+
+};
+
+
+/* Computes next move
+ * PARAM:
+ * - previous_move: the move from the previous player. If this is the
+ *                  first move of the game, `previous_move` is equal to
+                    a starting tile played by player_id == -1
+ * - tile:          the tile that the player is supposed to play
+ * RETURNS:
+ * - the next move for the player.
+ */
+struct move_t play(const struct move_t previous_move, const struct tile_t tile);
+
+/* Clean up the resources the player has been using. Is called once at
+   the end of the game.
+ * POSTCOND:
+ * - every allocation done during the calls to initialize and play
+ *   functions must have been freed
+ */
+void finalize(){
+	destroy_board(graph_1board_t);
+
+
+}
