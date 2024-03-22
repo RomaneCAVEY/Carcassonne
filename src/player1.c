ccd929b2f61f268f612d091a0c436ab6db9ee3d3
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2024-03-22 17:57:43 +0100

    player_1

--- src/player1.c
+++ src/player1.c
@@ -1,14 +1,14 @@
 #include "player.h"
 #include "board.h"
 
- board_t raph_1={};
+ board_t board_1={};
 
 char const* get_player_name(){
 	return "Player_1";
 }
 
 void initialize(unsigned int player_id, const struct move_t first_move, struct gameconfig_t config) {
-	graph_1= init_board(first_move);
+	board_1= init_board(first_move);
 
 };
 
@@ -22,7 +22,10 @@ void initialize(unsigned int player_id, const struct move_t first_move, struct g
  * RETURNS:
  * - the next move for the player.
  */
-struct move_t play(const struct move_t previous_move, const struct tile_t tile);
+struct move_t play(const struct move_t previous_move, const struct tile_t tile){
+	board_add_tile(tile, board_1);
+
+}
 
 /* Clean up the resources the player has been using. Is called once at
    the end of the game.
@@ -31,7 +34,5 @@ struct move_t play(const struct move_t previous_move, const struct tile_t tile);
  *   functions must have been freed
  */
 void finalize(){
-	destroy_board(graph_1board_t);
-
-
-}
+	destroy_board(board_1);
+	}
