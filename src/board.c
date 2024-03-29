085ae3247681a1177afbbe8b2af8aff1f19c80d4
Alexis Brandner <alexis.brandner@enseirb-matmeca.fr>
2024-03-29 15:33:48 +0100

    clean: Apply clang-format (WebKit) to board

--- src/board.c
+++ src/board.c
@@ -4,44 +4,46 @@
 #define BOARD_CENTER 101
 
 struct board_t {
-  struct tile_t** tiles;
+    struct tile_t **tiles;
 };
 
-struct board_t* board_init(struct tile_t tile) {
+struct board_t *board_init(struct tile_t tile)
+{
 
-  struct board_t *board = malloc(sizeof(struct board_t));
+    struct board_t *board = malloc(sizeof(struct board_t));
 
-  for (int i = 0; i < BOARD_SIZE; i++) {
-    board->tiles[i] = malloc(sizeof(struct tile_t) * 201);
-    for (int j = 0; j < BOARD_SIZE; j++) {
-      board->tiles[i][j] = CARC_TILE_EMPTY;
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board->tiles[i] = malloc(sizeof(struct tile_t) * 201);
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            board->tiles[i][j] = CARC_TILE_EMPTY;
+        }
     }
-  }
 
-  board->tiles[BOARD_CENTER][BOARD_CENTER] = tile;
+    board->tiles[BOARD_CENTER][BOARD_CENTER] = tile;
 
-  return board;
+    return board;
 }
 
-int board_add(struct board_t* board, struct tile_t tile, int x, int y) {
-  if (board->tiles[BOARD_CENTER + x][BOARD_CENTER + y] != CARC_TILE_EMPTY)
-    return 0;
+int board_add(struct board_t *board, struct tile_t tile, int x, int y)
+{
+    if (board->tiles[BOARD_CENTER + x][BOARD_CENTER + y] != CARC_TILE_EMPTY)
+        return 0;
 
-  board->tiles[BOARD_CENTER + x][BOARD_CENTER + y] = tile;
+    board->tiles[BOARD_CENTER + x][BOARD_CENTER + y] = tile;
 
-  return 1;
+    return 1;
 }
 
-struct tile_t board_get(struct board_t* board, int x, int y) {
-  return board->tiles[BOARD_CENTER + x][BOARD_CENTER + y];
+struct tile_t board_get(struct board_t *board, int x, int y)
+{
+    return board->tiles[BOARD_CENTER + x][BOARD_CENTER + y];
 }
 
-void board_free(struct board_t* board) {
-  for (int i = 0; i < BOARD_SIZE; i++) {
-    free(board->tiles[i]);
-  }
-  free(board);
-  board = NULL;
+void board_free(struct board_t *board)
+{
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        free(board->tiles[i]);
+    }
+    free(board);
+    board = NULL;
 }
-
-
--- src/board.h
+++ src/board.h
@@ -5,15 +5,15 @@
 
 struct board_t;
 
-struct board_t* board_init(struct tile_t tile);
+struct board_t *board_init(struct tile_t tile);
 
 /* Retourne 0 si ajout impossible, 1 si ajouté avec succès.
  *
  */
-int board_add(struct board_t* board, struct tile_t tile, int x, int y);
+int board_add(struct board_t *board, struct tile_t tile, int x, int y);
 
-struct tile_t board_get(struct board_t* board, int x, int y);
+struct tile_t board_get(struct board_t *board, int x, int y);
 
-void board_free(struct board_t* board);
+void board_free(struct board_t *board);
 
 #endif // __BOARD_H__
