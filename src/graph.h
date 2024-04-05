1c09d77c5ee8cfeb0fb0b80c6df8bc929fb7bb35
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2024-04-05 15:27:46 +0200

    add clients player1.c + player2.c

--- src/graph.h
+++ src/graph.h
@@ -1,3 +1,6 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
 #include <igraph.h>
 #include <stdio.h>
 #include "move.h"
@@ -8,8 +11,15 @@
 igraph_t transform_tile_to_graph(struct tile_t);
 
 
+//Init the graph with the first tile
+igraph_t init_graph(struct tile_t t);
 
 //Add a tile which is transformed in graph to the main graph and add the colors of the vertices to the tab color
 //Use realloc if colors is full (but no size so...)
-igraph_t add_tile_to_graph(struct tile_t tile,igraph_t g,struct board, enum color_t* color);
+igraph_t graph_add(struct tile_t tile,igraph_t g,struct board, enum color_t* color);
 
+
+//free graph
+void free_graph(igraph_t graphe);
+
+#endif
--- src/player2.c
+++ src/player2.c
@@ -8,24 +8,24 @@
 
 
 #ifndef BOARD_SIZE
-#define BOARD_SIZE 201
+#define BOARD_SIZE 202
 #endif
 
 
 
 //VARIABLE GLOBALE
-struct board_t board_1={};
-igraph_t graph_1={};
-struct gameconfig_t config_1={};
+struct board_t board_2={};
+igraph_t graph_2={};
+struct gameconfig_t config_2={};
 
 char const* get_player_name(){
-	return "Player_1";
+	return "Player_2";
 }
 
 void initialize(unsigned int player_id, const struct move_t first_move, struct gameconfig_t config) {
-	board_1= board_init(first_move.tile);
-	graph_1=init_graph(first_move.tile);
-	config_1=config;
+	board_2= board_init(first_move.tile);
+	graph_2=init_graph(first_move.tile);
+	config_2=config;
 }
 
 
@@ -33,7 +33,7 @@ void initialize(unsigned int player_id, const struct move_t first_move, struct g
  * PARAM:
  * - previous_move: the move from the previous player. If this is the
  *                  first move of the game, `previous_move` is equal to
-                    a starting tile played by player_id == -1
+                    a starting tile played by player_id == -2
  * - tile:          the tile that the player is supposed to play
  * RETURNS:
  * - the next move for the player.
@@ -42,14 +42,14 @@ void initialize(unsigned int player_id, const struct move_t first_move, struct g
 
 
 struct move_t play(const struct move_t previous_move, const struct tile_t tile){
-	board_add(board_1,previous_move.tile,previous_move.x,previous_move.y);
-	graph_add(tile, graph_1);
+	board_add(board_2,previous_move.tile,previous_move.x,previous_move.y);
+	graph_add(tile, graph_2);
 	struct move_t current_move={};
-	current_move.player_id=1;
+	current_move.player_id=2;
 	for (int i=0;i<BOARD_SIZE;i++){
 		for (int j=0;j<BOARD_SIZE;j++){
-			if(board_get(board_1, i ,j)!=CARC_TILE_EMPTY){
-				if (board_get(board_1, i-1, j)!=CARC_TILE_EMPTY || board_get(board_1, i, j+1)!=CARC_TILE_EMPTY ||  board_get(board_1, i+1, j)!=CARC_TILE_EMPTY ||  board_get(board_1, i, j-1)!=CARC_TILE_EMPTY){
+			if(board_get(board_2, i ,j)!=CARC_TILE_EMPTY){
+				if (board_get(board_2, i-1, j)!=CARC_TILE_EMPTY || board_get(board_2, i, j+1)!=CARC_TILE_EMPTY ||  board_get(board_2, i+1, j)!=CARC_TILE_EMPTY ||  board_get(board_2, i, j-1)!=CARC_TILE_EMPTY){
 					current_move.x=i;
 					current_move.y=j;
 
@@ -59,8 +59,8 @@ struct move_t play(const struct move_t previous_move, const struct tile_t tile){
 	}
 
 	current_move.tile=tile;
-	board_add( board_1,current_move.tile,current_move.x,current_move.y);
-	graph_add(tile, graph_1);
+	board_add( board_2,current_move.tile,current_move.x,current_move.y);
+	graph_add(tile, graph_2);
 	return current_move;
 
 }
@@ -72,7 +72,7 @@ struct move_t play(const struct move_t previous_move, const struct tile_t tile){
  *   functions must have been freed
  */
 void finalize(){
-	board_free(board_1);
-	free_graph(graph_1);
-	deck_free(config_1.deck);
+	board_free(board_2);
+	free_graph(graph_2);
+	deck_free(config_2.deck);
 	}
