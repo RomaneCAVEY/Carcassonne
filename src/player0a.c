f3888391e60189a0f2ca064c4e78ad9c09745ae4
Alexis Brandner <alexis.brandner@enseirb-matmeca.fr>
2024-04-12 19:34:02 +0200

    [dev] refactor(player0): Optimized the algorithm that searches for a position to place the tile

--- src/player0a.c
+++ src/player0a.c
@@ -1,80 +1,92 @@
 #include "board.h"
 #include "deck.h"
+#include "extended_player.h"
 #include "move.h"
 #include "player.h"
 #include "struct_board.h"
 #include "tile.h"
-#include "extended_player.h"
-#include <stdlib.h>
 #include <stdio.h>
-
-#ifndef BOARD_SIZE
-#define BOARD_SIZE 201
-#endif
-
-#ifndef BOARD_CENTER
-#define BOARD_CENTER 101
-#endif
-
+#include <stdlib.h>
 
 // VARIABLE GLOBALE
 struct board_t *board_1 = NULL;
 struct gameconfig_t config_1 = {};
 
+int p1_board_min_x = 0;
+int p1_board_max_x = 0;
+int p1_board_min_y = 0;
+int p1_board_max_y = 0;
+
 char const *get_player_name() { return "Player_0a"; }
 
 void initialize(unsigned int player_id, const struct move_t first_move,
-    struct gameconfig_t config)
-{
-    board_1 = board_init(first_move.tile);
-    config_1 = config;
-    printf("Player %u initialized!\n", player_id);
+                struct gameconfig_t config) {
+  board_1 = board_init(first_move.tile);
+  config_1 = config;
+  printf("Player %u initialized!\n", player_id);
 }
 
-/* Computes next move
- * PARAM:
- * - previous_move: the move from the previous player. If this is the
- *                  first move of the game, `previous_move` is equal to
-                    a starting tile played by player_id == -1
- * - tile:          the tile that the player is supposed to play
- * RETURNS:
- * - the next move for the player.
- */
+void update_board_bounds(struct move_t move) {
+  if (move.x > p1_board_max_x && p1_board_max_x < BOARD_CENTER - 1)
+    p1_board_max_x = move.x;
 
-struct move_t play(const struct move_t previous_move, const struct tile_t tile)
-{
-    board_add(board_1, previous_move.tile, previous_move.x, previous_move.y);
-    struct move_t current_move = {};
-    current_move.player_id = 1;
-    for (int i=-BOARD_CENTER+1;i<BOARD_CENTER;i++){
-    for (int j=-BOARD_CENTER+1;j<BOARD_CENTER;j++){
-      if(board_add_check(board_1, tile, i, j)){
-	 	 current_move.x=i;
-	 	 current_move.y=j;
+  if (move.y > p1_board_max_y && p1_board_max_y < BOARD_CENTER - 1)
+    p1_board_max_y = move.y;
 
+  if (move.x < p1_board_min_x && p1_board_min_x > 2 - BOARD_CENTER)
+    p1_board_min_x = move.x;
+
+  if (move.y < p1_board_min_y && p1_board_min_y > 2 - BOARD_CENTER)
+    p1_board_min_y = move.y;
+}
+
+struct move_t play(const struct move_t previous_move,
+                   const struct tile_t tile) {
+  board_add(board_1, previous_move.tile, previous_move.x, previous_move.y);
+  update_board_bounds(previous_move);
+
+  struct move_t current_move = {};
+  current_move.player_id = 1;
+
+  for (int i = p1_board_min_y - 1; i < p1_board_max_y + 1; i++) {
+    for (int j = p1_board_min_x - 1; j < p1_board_max_x + 1; j++) {
+      if (board_add_check(board_1, tile, i, j)) {
+        current_move.x = i;
+        current_move.y = j;
+        // Note: We're using the last of all possible placements. This can be
+        // further optimized.
       }
     }
   }
 
-    current_move.tile = tile;
-    board_add(board_1, current_move.tile, current_move.x, current_move.y);
-    return current_move;
+  current_move.tile = tile;
+
+  board_add(board_1, current_move.tile, current_move.x, current_move.y);
+  update_board_bounds(current_move);
+
+  return current_move;
 }
 
-int is_place_available(struct board_t *board,int i, int j,struct tile_t tile){
+int is_place_available(struct board_t *board, int i, int j,
+                       struct tile_t tile) {
+  if (!(compare_tile(board_get(board, i - 1, j), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, WEST)))
+    return 1;
 
-	if (!(compare_tile(board_get(board, i-1, j),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, WEST)))
-		return 1;
-	if (!(compare_tile(board_get(board, i, j+1),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, NORTH)))
-		return 1;
-	if (!(compare_tile(board_get(board, i, j-1),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, SOUTH)))
-		return 1;
-	if (!(compare_tile(board_get(board, i+1, j),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, EAST)))
-		return 1;
-	return 0;
+  if (!(compare_tile(board_get(board, i, j + 1), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, NORTH)))
+    return 1;
 
+  if (!(compare_tile(board_get(board, i, j - 1), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, SOUTH)))
+    return 1;
+
+  if (!(compare_tile(board_get(board, i + 1, j), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, EAST)))
+    return 1;
+
+  return 0;
 }
-
 
 /* Clean up the resources the player has been using. Is called once at
    the end of the game.
@@ -82,8 +94,7 @@ int is_place_available(struct board_t *board,int i, int j,struct tile_t tile){
  * - every allocation done during the calls to initialize and play
  *   functions must have been freed
  */
-void finalize()
-{
-    board_free(board_1);
-    deck_free(config_1.deck);
+void finalize() {
+  board_free(board_1);
+  deck_free(config_1.deck);
 }
--- src/player0b.c
+++ src/player0b.c
@@ -1,72 +1,92 @@
 #include "board.h"
 #include "deck.h"
+#include "extended_player.h"
 #include "move.h"
 #include "player.h"
 #include "struct_board.h"
 #include "tile.h"
-#include "extended_player.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 // VARIABLE GLOBALE
 struct board_t *board_2 = NULL;
 struct gameconfig_t config_2 = {};
 
+int p2_board_min_x = 0;
+int p2_board_max_x = 0;
+int p2_board_min_y = 0;
+int p2_board_max_y = 0;
+
 char const *get_player_name() { return "Player_0b"; }
 
 void initialize(unsigned int player_id, const struct move_t first_move,
-    struct gameconfig_t config)
-{
-    board_2 = board_init(first_move.tile);
-    config_2 = config;
-    printf("Player %u initialized!\n", player_id);
+                struct gameconfig_t config) {
+  board_2 = board_init(first_move.tile);
+  config_2 = config;
+  printf("Player %u initialized!\n", player_id);
 }
 
-/* Computes next move
- * PARAM:
- * - previous_move: the move from the previous player. If this is the
- *                  first move of the game, `previous_move` is equal to
-                    a starting tile played by player_id == -1
- * - tile:          the tile that the player is supposed to play
- * RETURNS:
- * - the next move for the player.
- */
+void update_board_bounds(struct move_t move) {
+  if (move.x > p2_board_max_x && p2_board_max_x < BOARD_CENTER - 1)
+    p2_board_max_x = move.x;
 
-struct move_t play(const struct move_t previous_move, const struct tile_t tile)
-{
-    board_add(board_2, previous_move.tile, previous_move.x, previous_move.y);
-    struct move_t current_move = {};
-    current_move.player_id = 1;
-   for (int i=-BOARD_CENTER+1;i<BOARD_CENTER;i++){
-    for (int j=-BOARD_CENTER+1;j<BOARD_CENTER;j++){
-      if(board_add_check(board_2, tile, i, j)){
-	 	 current_move.x=i;
-	 	 current_move.y=j;
+  if (move.y > p2_board_max_y && p2_board_max_y < BOARD_CENTER - 1)
+    p2_board_max_y = move.y;
 
+  if (move.x < p2_board_min_x && p2_board_min_x > 2 - BOARD_CENTER)
+    p2_board_min_x = move.x;
+
+  if (move.y < p2_board_min_y && p2_board_min_y > 2 - BOARD_CENTER)
+    p2_board_min_y = move.y;
+}
+
+struct move_t play(const struct move_t previous_move,
+                   const struct tile_t tile) {
+  board_add(board_2, previous_move.tile, previous_move.x, previous_move.y);
+  update_board_bounds(previous_move);
+
+  struct move_t current_move = {};
+  current_move.player_id = 1;
+
+  for (int i = p2_board_min_y - 1; i < p2_board_max_y + 1; i++) {
+    for (int j = p2_board_min_x - 1; j < p2_board_max_x + 1; j++) {
+      if (board_add_check(board_2, tile, i, j)) {
+        current_move.x = i;
+        current_move.y = j;
+        // Note: We're using the last of all possible placements. This can be
+        // further optimized.
       }
     }
   }
-    current_move.tile = tile;
-    board_add(board_2, current_move.tile, current_move.x, current_move.y);
-    return current_move;
+
+  current_move.tile = tile;
+
+  board_add(board_2, current_move.tile, current_move.x, current_move.y);
+  update_board_bounds(current_move);
+
+  return current_move;
 }
 
+int is_place_available(struct board_t *board, int i, int j,
+                       struct tile_t tile) {
+  if (!(compare_tile(board_get(board, i - 1, j), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, WEST)))
+    return 1;
 
-int is_place_available(struct board_t *board,int i, int j,struct tile_t tile){
+  if (!(compare_tile(board_get(board, i, j + 1), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, NORTH)))
+    return 1;
 
-	if (!(compare_tile(board_get(board, i-1, j),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, WEST)))
-		return 1;
-	if (!(compare_tile(board_get(board, i, j+1),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, NORTH)))
-		return 1;
-	if (!(compare_tile(board_get(board, i, j-1),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, SOUTH)))
-		return 1;
-	if (!(compare_tile(board_get(board, i+1, j),CARC_TILE_EMPTY) && tile_check(board_get(board, i-1, j), tile, EAST)))
-		return 1;
-	return 0;
+  if (!(compare_tile(board_get(board, i, j - 1), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, SOUTH)))
+    return 1;
 
+  if (!(compare_tile(board_get(board, i + 1, j), CARC_TILE_EMPTY) &&
+        tile_check(board_get(board, i - 1, j), tile, EAST)))
+    return 1;
+
+  return 0;
 }
-
 
 /* Clean up the resources the player has been using. Is called once at
    the end of the game.
@@ -74,8 +94,7 @@ int is_place_available(struct board_t *board,int i, int j,struct tile_t tile){
  * - every allocation done during the calls to initialize and play
  *   functions must have been freed
  */
-void finalize()
-{
-    board_free(board_2);
-    deck_free(config_2.deck);
+void finalize() {
+  board_free(board_2);
+  deck_free(config_2.deck);
 }
