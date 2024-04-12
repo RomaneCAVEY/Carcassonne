9fd6ff7ddaee8c26648899d609b9c12a7b7282a9
Gael Valade <gael.valade@enseirb-matmeca.fr>
2024-04-12 14:49:59 +0200

    [dev](feat) : update .h

--- src/super_board.h
+++ src/super_board.h
@@ -2,6 +2,7 @@
 #define __SUPER__BOARD__H
 
 #include "board.h"
+#include "graph.h"
 #include "move.h"
 
 struct utils_graph_t{
@@ -13,6 +14,7 @@ struct utils_graph_t{
 // the size of list is size
 struct super_board_t{
 	struct board_t *board;
+	igraph_t graph;
 	struct utils_graph_t *list;
 	enum color_t* colors;
 	int capacite;
@@ -26,6 +28,7 @@ void init_super_board(struct tile_t tile, struct super_board_t* super_board) ;
 // TO DO :
 // facteur *13 pour la capcité pour les couleurs par rapport à list
 void add_tile_to_super_board(struct tile_t tile, struct super_board_t super_board, int x,int y);
+
 
 //Add the color of the tile in the board
 void add_color(int sommet, enum color_t c, struct super_board_t* super_board);
