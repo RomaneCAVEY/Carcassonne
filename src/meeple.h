1af5bb13f69085567ab9c6e5670eaaa3ab3af08d
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2024-04-19 18:10:54 +0200

    [meeple] feat : comments in meeple.h

--- src/meeple.h
+++ src/meeple.h
@@ -1,8 +1,14 @@
 #ifndef __MEEPLE_H__
 #define __MEEPLE_H__
 
+/* Struct meeple
+ * @param:
+ * -The tab player1 has all the vertices where the player 1 has a meeple, same for player2
+ *- The size is the size of each tab
+ *- The capacity is the capcity of each tab 
+*/
 
-struct meeple{
+struct meeple_t{
 	int* player1;
 	int* player2;
 	int size1;
@@ -12,9 +18,23 @@ struct meeple{
 }
 
 
+/*@ Init the struct meeple
+ * @param: the capacity(int) of the both tab player
+ * @return: a struct meeple with empty param
+ */
+struct meeple_t init_meeple(int capacity);
 
-struct meeple init_meeple();
 
-struct 
+/*Add the meeple played in the move
+ *@param: the meeple and the move 
+ *@return: the meeple with the new meeple according to the player who played 
+ * */
+void add_meeple(stuct meeple_t *meeple, struct move_t move);
 
+
+
+
+/*free the tabs player1 and player2 of the meeple
+ * */
+void free_meepple(struct meeple_t meeple);
 #endif
