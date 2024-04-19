25624cfec666fa19623117a3926670401d05bf22
Romane Cavey <romane.cavey@enseirb-matmeca.fr>
2024-04-19 17:39:57 +0200

    [meeple] feat : meeple.h

---
+++ src/meeple.h
@@ -0,0 +1,20 @@
+#ifndef __MEEPLE_H__
+#define __MEEPLE_H__
+
+
+struct meeple{
+	int* player1;
+	int* player2;
+	int size1;
+	int size2;
+	int capacity1;
+	int capacity2;
+}
+
+
+
+struct meeple init_meeple();
+
+struct 
+
+#endif
